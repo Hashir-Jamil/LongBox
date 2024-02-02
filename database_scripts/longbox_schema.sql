90591a4db62dafc226a910419d6737e7d5fade34
Hashir-Jamil <hashir.jamil17@gmail.com>
2024-02-02 13:21:09 -0500

    [ADD] SQL Script for longbox database schema

---
+++ database_scripts/longbox_schema.sql
@@ -0,0 +1,46 @@
+CREATE TABLE "user" (
+  "id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "user_name" text UNIQUE,
+  "first_name" text,
+  "last_name" text,
+  "dob" date,
+  "address" text,
+  "email" text,
+  "join_date" date
+);
+
+CREATE TABLE "comic_book" (
+  "id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "issue_title" text,
+  "series_title" text,
+  "author" text,
+  "volume_number" integer,
+  "issues_number" integer,
+  "publisher" text,
+  "year" date,
+  "date_added_db" date
+);
+
+CREATE TABLE "comic_book_list" (
+  "user_id" integer NOT NULL,
+  "comic_book_id" integer NOT NULL,
+  "date_added_user_list" date
+  PRIMARY KEY ("user_id","comic_book_id")
+);
+
+CREATE TABLE "comments" (
+  "id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "message" text,
+  "comment_date" date,
+  "comic_book_id" integer,
+  "user_id" integer,
+  "user_name" text
+);
+
+ALTER TABLE "comic_book_list" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+
+ALTER TABLE "comic_book_list" ADD FOREIGN KEY ("comic_book_id") REFERENCES "comic_book" ("id");
+
+ALTER TABLE "comments" ADD FOREIGN KEY ("comic_book_id") REFERENCES "user" ("id");
+
+ALTER TABLE "comments" ADD FOREIGN KEY ("user_id") REFERENCES "comic_book" ("id");
