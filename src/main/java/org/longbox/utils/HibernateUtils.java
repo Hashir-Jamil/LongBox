b8fe772564f48a05832d86cdfd9e62709b538fe6
Hashir Jamil <hashir.jamil17@gmail.com>
2024-02-03 12:50:28 -0500

    [ADD] Entity classes to hibernate configuration

--- src/main/java/org/longbox/utils/HibernateUtils.java
+++ src/main/java/org/longbox/utils/HibernateUtils.java
@@ -3,6 +3,9 @@ package org.longbox.utils;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.longbox.authentication.User;
+import org.longbox.comiccollection.ComicBookList;
+import org.longbox.comics.ComicBook;
+import org.longbox.comments.Comment;
 
 public class HibernateUtils {
     private static SessionFactory sessionFactory;
@@ -11,7 +14,10 @@ public class HibernateUtils {
         if (sessionFactory == null) {
             Configuration configuration = new Configuration()
                     .configure("hibernate.cfg.xml")
-                    .addAnnotatedClass(User.class);
+                    .addAnnotatedClass(User.class)
+                    .addAnnotatedClass(ComicBook.class)
+                    .addAnnotatedClass(ComicBookList.class)
+                    .addAnnotatedClass(Comment.class);
             sessionFactory = configuration.buildSessionFactory();
         }
         return sessionFactory;
