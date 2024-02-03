de8a72d11954514dbe9b475958e2bae033935af3
ahan10 <ahan10@my.yoru.ca>
2024-02-03 16:37:13 -0500

    [ADD] Completed the user class

--- src/main/java/org/longbox/authentication/User.java
+++ src/main/java/org/longbox/authentication/User.java
@@ -15,7 +15,7 @@ public class User {
     @Column(name = "user_name")
     private String userName;
 
-    @Column(name = "first_name")
+	@Column(name = "first_name")
     private String firstName;
 
     @Column(name = "last_name")
@@ -23,11 +23,68 @@ public class User {
 
     @Column(name = "dob")
     private Date dob;
+    
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "passowrd")
+    private String password;
 
     @Column(name = "country")
     private String country;
 
     @Column(name = "join_date")
     private Date joinDate;
+    
+    public User() {
+    	
+    }
+
+	public User(String userName, String firstName, String lastName, Date dob, String email, String password,
+			String country, Date joinDate) {
+		super();
+		this.userName = userName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.email = email;
+		this.password = password;
+		this.country = country;
+		this.joinDate = new Date();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public Date getJoinDate() {
+		return joinDate;
+	}
+    
+   
 
 }
