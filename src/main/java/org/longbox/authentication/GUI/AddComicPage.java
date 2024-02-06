14c8fb0b8741aafd8bf145324475e43340c947a8
Ali Sina <ali.sina1447@gmail.com>
2024-02-06 16:02:11 -0500

    [ADD] Created GUI of Add Comic Page

---
+++ src/main/java/org/longbox/authentication/GUI/AddComicPage.java
@@ -0,0 +1,26 @@
+package org.longbox.authentication.GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
+public class AddComicPage extends JFrame implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
