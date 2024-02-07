bd18d1da2d8805bcf50e8f498eb77a6734b614e2
Hashir-Jamil <hashir.jamil17@gmail.com>
2024-02-07 12:37:39 -0500

    [ADD] GUI Page for adding a comic book to the repository.

---
+++ src/main/java/org/longbox/comics/GUI/AddComic.java
@@ -0,0 +1,34 @@
+package org.longbox.comics.GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import javax.swing.JTextField;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+
+public class AddComic extends JFrame implements ActionListener {
+	public AddComic() {
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new CardLayout(0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1, "name_1404782606058300");
+		panel_1.setLayout(new FormLayout(new ColumnSpec[] {},
+			new RowSpec[] {}));
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
