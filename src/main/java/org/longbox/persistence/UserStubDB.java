8ed26542419a00133412d498e8fe37c9bf2a24d4
Hashir-Jamil <hashir.jamil17@gmail.com>
2024-02-07 23:59:33 -0500

    [ADD] json serializer and deserializer using gson for stub user json db file.

--- src/main/java/org/longbox/persistence/UserStubDB.java
+++ src/main/java/org/longbox/persistence/UserStubDB.java
@@ -1,15 +1,23 @@
 package org.longbox.persistence;
 
+import java.io.*;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import lombok.NoArgsConstructor;
 import org.longbox.domainobjects.UserDTO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+@NoArgsConstructor
 public class UserStubDB {
 
     private List<UserDTO> userStubData = new ArrayList<>();
 
-    public UserStubDB() {
+    public void loadUsers() {
         UserDTO u1 = new UserDTO("Always_Scheming",
                 "John",
                 "Smith",
@@ -40,4 +48,30 @@ public class UserStubDB {
                 new Date());
         userStubData.add(u3);
     }
+
+    public void serializeUserStubDB() {
+        String json = new Gson().toJson(userStubData);
+        String file = "src/main/resources/UserStubDB.json";
+        try (PrintStream out = new PrintStream(new FileOutputStream(file))) {
+            out.print(json);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<UserDTO> deserializeUserStubDB(String filepath) {
+        Type listType = new TypeToken<ArrayList<UserDTO>>(){}.getType();
+        JsonReader reader = null;
+
+        try {
+            reader = new JsonReader(new FileReader(filepath));
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<UserDTO> dummyUsers = new Gson().fromJson(reader, listType);
+        return dummyUsers;
+    }
+
 }
