fb842fa072caa80d5592e18bba40bd4d3203f501
Hashir-Jamil <hashir.jamil17@gmail.com>
2024-02-12 16:26:58 -0500

    [ADD] comments stub database class

---
+++ src/main/java/org/longbox/persistence/stubdatabase/CommentStub.java
@@ -0,0 +1,37 @@
+package org.longbox.persistence.stubdatabase;
+
+import org.longbox.domainobjects.dto.CommentDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommentStub {
+
+    private static List<CommentDTO> commentsStubData = new ArrayList<>();
+
+    public static void loadComments() {
+
+        CommentDTO comment1 = new CommentDTO(
+                "This comic was really good",
+                "Always_Scheming",
+                "Spider Man"
+        );
+        commentsStubData.add(comment1);
+
+        CommentDTO comment2 = new CommentDTO(
+                "This comic was really bad",
+                "Always_Throwing",
+                "Spider man"
+        );
+        commentsStubData.add(comment2);
+
+        CommentDTO comment3 = new CommentDTO(
+                "This comic changed my life",
+                "Phoenix",
+                "Arkham Asylum"
+        );
+        commentsStubData.add(comment3);
+
+    }
+
+}
