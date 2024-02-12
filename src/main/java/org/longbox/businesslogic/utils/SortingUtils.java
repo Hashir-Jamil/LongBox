c93ff03bcb086b72857065ba731c69fc81e40d53
Hashir-Jamil <hashir.jamil17@gmail.com>
2024-02-12 00:28:29 -0500

    [REFACTOR] Moved sorting methods from UserDTO class to business logic package's utils subpacakage

---
+++ src/main/java/org/longbox/businesslogic/utils/SortingUtils.java
@@ -0,0 +1,24 @@
+package org.longbox.businesslogic.utils;
+
+import org.longbox.businesslogic.comparators.ComicBookDateAddeddComparator;
+import org.longbox.businesslogic.comparators.ComicBookNameComparator;
+import org.longbox.businesslogic.comparators.ComicBookYearPublishedComparator;
+import org.longbox.domainobjects.ComicBookDTO;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SortingUtils {
+
+    public static void sortComicsAToZ(List<ComicBookDTO> comicBookList) {
+        Collections.sort(comicBookList, new ComicBookNameComparator());
+    }
+
+    public static void sortComicsDateAdded(List<ComicBookDTO> comicBookList) {
+        Collections.sort(comicBookList, new ComicBookDateAddeddComparator());
+    }
+
+    public static void sortComicsYearPublished(List<ComicBookDTO> comicBookList) {
+        Collections.sort(comicBookList, new ComicBookYearPublishedComparator());
+    }
+}
