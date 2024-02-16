31959d0003dad9e4a581ec38be8fdcaabe7df27b
Hashir Jamil <hashir.jamil17@gmail.com>
2024-02-15 21:05:53 -0500

    [ADD] updating testing files for domain objects

---
+++ src/test/java/org/longbox/domainobjects/UserDTOTest.java
@@ -0,0 +1,9 @@
+package org.longbox.domainobjects;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.longbox.domainobjects.dto.UserDTO;
+
+public class UserDTOTest {
+
+}
