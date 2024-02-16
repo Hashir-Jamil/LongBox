6347ebdf3a45c75c9b375d9617abc58c35f3d7bd
ahan10 <ahanbhargava10@gmail.com>
2024-02-16 00:35:52 -0500

    [REFACTOR] Removed unused import statements.

--- src/test/java/org/longbox/businesslogic/UserSessionTest.java
+++ src/test/java/org/longbox/businesslogic/UserSessionTest.java
@@ -1,8 +1,5 @@
 package org.longbox.businesslogic;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.longbox.businesslogic.UserSession;
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
