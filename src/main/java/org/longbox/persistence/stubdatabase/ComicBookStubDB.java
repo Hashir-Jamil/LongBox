d2a21cf86d0434a58245ec606820c4d30e02a15d
SupaHacka02 <mherericgyulumyan@gmail.com>
2024-02-15 23:54:33 -0500

    [ADD] More comic books to stub database

--- src/main/java/org/longbox/persistence/stubdatabase/ComicBookStubDB.java
+++ src/main/java/org/longbox/persistence/stubdatabase/ComicBookStubDB.java
@@ -69,6 +69,90 @@ public class ComicBookStubDB {
                 1993
         );
         comicBookStubData.add(comicBook4);
+        
+        ComicBookDTO comicBook5 = new ComicBookDTO(
+                "Winter Wolrd",
+                "Chuck Dixon",
+                "Jorge Zaffino",
+                new String[] {"Adventure", "Post-Apocalyptic", "Gunslinger"},
+                "Description",
+                3,
+                "Eclipse",
+                1987
+        );
+        comicBookStubData.add(comicBook5);
+        
+        ComicBookDTO comicBook6 = new ComicBookDTO(
+                "Hellhounds Panzer Cops",
+                "Mamoru Oshii",
+                "Kamui Fujiwara",
+                new String[] {"Military", "Police", "Adventure", "Dystopian", "Manga"},
+                "Description",
+                6,
+                "Dark Horse",
+                1994
+        );
+        comicBookStubData.add(comicBook6);
+        
+        ComicBookDTO comicBook7 = new ComicBookDTO(
+                "Jon Sable Freelance (1983)",
+                "Mike Grell",
+                "Mike Grell",
+                new String[] {"Action", "Adventure", "Crime", "Vigilantes"},
+                "Description",
+                56,
+                "First Comics",
+                1983
+        );
+        comicBookStubData.add(comicBook7);
+        
+        ComicBookDTO comicBook8 = new ComicBookDTO(
+                "Chronicles of Corum",
+                "Mike Baron",
+                "Mike Mignola",
+                new String[] {"Action", "Adventure", "Sword and Socery"},
+                "Description",
+                12,
+                "First Comics",
+                1987
+        );
+        comicBookStubData.add(comicBook8);
+        
+        ComicBookDTO comicBook9 = new ComicBookDTO(
+                "Drakuun",
+                "Johji Manabe",
+                "Johnji Manabe",
+                new String[] {"Adventure", "Sword and Socery", "Comedy", "Manga"},
+                "Description",
+                24,
+                "Dark Horse",
+                1997
+        );
+        comicBookStubData.add(comicBook9);
+        
+        ComicBookDTO comicBook10 = new ComicBookDTO(
+                "Sojourn",
+                "Ron Marz",
+                "Greg Land",
+                new String[] {"Action", "Adventure", "Sword and Sorcery"},
+                "Description",
+                34,
+                "Crossgen",
+                2001
+        );
+        comicBookStubData.add(comicBook10);
+        
+        ComicBookDTO comicBook11 = new ComicBookDTO(
+                "Trekker (1988)",
+                "Ron Randall",
+                "Ron Randall",
+                new String[] {"Action", "Crime", "Vigilantes", "Dystopian"},
+                "Description",
+                6,
+                "Dark Horse",
+                1988
+        );
+        comicBookStubData.add(comicBook11);
     }
 
     public void serializeComicBookStubDB() {
