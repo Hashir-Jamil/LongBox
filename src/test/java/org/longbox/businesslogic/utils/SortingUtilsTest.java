65592a389fb116a69047fd5b9bff789818939bec
Hashir Jamil <hashir.jamil17@gmail.com>
2024-02-15 23:18:11 -0500

    [HOTFIX] incorrect import statement fixed.

--- src/test/java/org/longbox/businesslogic/utils/SortingUtilsTest.java
+++ src/test/java/org/longbox/businesslogic/utils/SortingUtilsTest.java
@@ -2,16 +2,6 @@ package org.longbox.businesslogic.utils;
 
 public class SortingUtilsTest {
 
-//    public static void sortComicsAToZ(List<ComicBookDTO> comicBookList) {
-//        Collections.sort(comicBookList, new ComicBookNameComparator());
-//    }
-//
-//    public static void sortComicsDateAdded(List<ComicBookDTO> comicBookList) {
-//        Collections.sort(comicBookList, new ComicBookDateAddeddComparator());
-//    }
-//
-//    public static void sortComicsYearPublished(List<ComicBookDTO> comicBookList) {
-//        Collections.sort(comicBookList, new ComicBookYearPublishedComparator());
-//    }
+
 
 }
