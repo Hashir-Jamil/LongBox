e3ce27c883a48647e0099b1dd7a1c03219fa798f
ahan10 <ahanbhargava10@gmail.com>
2024-03-07 01:13:18 -0500

    [Delete] Removed unused code.

--- src/main/java/org/longbox/businesslogic/UserSession.java
+++ src/main/java/org/longbox/businesslogic/UserSession.java
@@ -49,41 +49,5 @@ public class UserSession {
     public String toString() {
         return user.toString();
     }
-    
-    
-
-    public static String generateUserProfileHTML (UserSession userSession) {
-        String htmlContent = String.format(
-                "<html><body><div><h1>Username: %s</h1><br" +
-                        "<h2>First Name: %s</h2><br" +
-                        "<h2>Last Name: %s</h2><br" +
-                        "<h2>Email Address: %s</h2><br" +
-                        "<h2>Date of Birth: %s</h2><br" +
-                        "<h2>Country: %s</h2><br" +
-                        "<h2>Join Date: %s</h2><br" +
-                        "</div></body></html><br",
-                userSession.getUser().getUserName(),
-                userSession.getUser().getFirstName(),
-                userSession.getUser().getLastName(),
-                userSession.getUser().getEmail(),
-                userSession.getUser().getDob().toString(),
-                userSession.getUser().getCountry(),
-                userSession.getUser().getJoinDate().toString()
-        );
-        return htmlContent;
-    }
-
-    public static String generateUserStatsHTML (UserSession userSession) {
-        String htmlContent = String.format(
-                "<html><body><div><h1>Comic Stats</h1><br" +
-                        "<h2>Comics Reading: %s</h2><br" +
-                        "<h2>Comics Finished: %s</h2><br" +
-                        "</div></body></html><br",
-                userSession.getUser().getComicsReading(),
-                userSession.getUser().getComicsFinished()
-                );
-        		return htmlContent;
-    }
-	
 
 }
