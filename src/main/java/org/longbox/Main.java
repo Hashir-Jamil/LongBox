2a8a8620de65accda695ef3c3907538d44a01cbd
Hashir Jamil <hashir.jamil17@gmail.com>
2024-03-07 13:23:55 -0500

    [DELETE] unused file and main code

--- src/main/java/org/longbox/Main.java
+++ src/main/java/org/longbox/Main.java
@@ -17,22 +17,5 @@ public class Main {
         // This entry point is used to test methods informally and
         // build stub database entries for the JSON files used with GSON
         // sample code is shown below in the two comment blocks.
-
-        /* UserStubDB userStubDB = new UserStubDB();
-        userStubDB.loadUsers();
-        userStubDB.serializeComicBookStubDB();
-
-        ComicBookStubDB comicBookStubDB = new ComicBookStubDB();
-        comicBookStubDB.loadComicBooks();
-        comicBookStubDB.serializeComicBookStubDB(); */
-        SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
-        Session session = sessionFactory.openSession();
-
-        Transaction transaction = session.beginTransaction();
-
-        Query query = session.createQuery("FROM User where id = 1");
-        List<User> userList = query.list();
-        System.out.println(userList.get(0).getUserName());
-
     }
 }
