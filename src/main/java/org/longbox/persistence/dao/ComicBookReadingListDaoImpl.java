2b87681087128f8ae534daad9b8e744e54ab6836
Hashir Jamil <hashir.jamil17@gmail.com>
2024-03-07 19:05:06 -0500

    [ADD] Finished & Reading comic books list database API methods

--- src/main/java/org/longbox/persistence/dao/ComicBookReadingListDaoImpl.java
+++ src/main/java/org/longbox/persistence/dao/ComicBookReadingListDaoImpl.java
@@ -1,29 +1,110 @@
 package org.longbox.persistence.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.longbox.businesslogic.exception.UserIDDoesNotExistException;
 import org.longbox.persistence.entity.ComicBook;
+import org.longbox.persistence.entity.ComicBookReadingList;
 import org.longbox.persistence.entity.User;
+import org.longbox.utils.HibernateUtils;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ComicBookReadingListDaoImpl implements ComicBookReadingListDao {
+    private SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
+
     @Override
-    public boolean saveToReading(long userId, long comicBookIdd) throws UserIDDoesNotExistException {
+    public boolean saveToReading(User user, ComicBook comicBook) throws UserIDDoesNotExistException {
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            user = (User) session.merge(user);
+            comicBook = (ComicBook) session.merge(comicBook);
+            ComicBookReadingList readingListItem = new ComicBookReadingList(user,comicBook);
+            session.persist(readingListItem);
+            transaction.commit();
+            return true;
+        }
+        catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+            if (session != null) {
+                session.close();
+            }
+        }
         return false;
     }
 
     @Override
-    public boolean removeFromReading(long userId, long comicBookId) {
-        return false;
+    public void removeFromReading(Long userId, Long comicBookId) {
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            Query query = session.createQuery("DELETE FROM ComicBookReadingList WHERE id.userId = :userId and id.comicBookId = :comicBookId");
+            query.setParameter("userId", userId);
+            query.setParameter("comicBookId", comicBookId);
+            query.executeUpdate();
+            transaction.commit();
+        }
+        catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
     }
 
     @Override
-    public List<ComicBook> getUsersReadingList(long userId) {
-        return null;
+    public List<ComicBook> getUsersReadingList(Long userId) {
+        Session session = null;
+
+        try {
+            session = sessionFactory.openSession();
+            Query<ComicBook> readingComicBooksList = session.createQuery("SELECT cbr.comicBook FROM ComicBookReadingList cbr WHERE cbr.user.id = :userId");
+            readingComicBooksList.setParameter("userId", userId);
+            return readingComicBooksList.getResultList();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
+        return Collections.emptyList();
     }
 
     @Override
-    public List<User> getListOfUsersReading(long comicBookId) {
-        return null;
+    public List<User> getListOfUsersReading(Long comicBookId) {
+        Session session = null;
+
+        try {
+            session = sessionFactory.openSession();
+            Query<User> allUsersWhoAreReading = session.createQuery("SELECT cbr.user FROM ComicBookReadingList cbr WHERE cbr.comicBook.id = :comicBookId");
+            allUsersWhoAreReading.setParameter("comicBookId", comicBookId);
+            return allUsersWhoAreReading.getResultList();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
+        return Collections.emptyList();
     }
 }
