44cd9398c022b6c7e446c8434c17ed1c61371ec0
ahan10 <ahanbhargava10@gmail.com>
2024-03-10 17:37:53 -0400

    [ADD] Added comment stub data

--- src/main/java/org/longbox/persistence/stubdatabase/CommentStubDb.java
+++ src/main/java/org/longbox/persistence/stubdatabase/CommentStubDb.java
@@ -1,11 +1,20 @@
 package org.longbox.persistence.stubdatabase;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import lombok.Getter;
 import lombok.Setter;
 import org.longbox.domainobjects.dto.CommentDto;
+import org.longbox.domainobjects.dto.UserDto;
 import org.longbox.persistence.dao.CommentDao;
 import org.longbox.persistence.entity.Comment;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 @Getter
@@ -15,7 +24,7 @@ public class CommentStubDb implements CommentDao {
     private final String ABSOLUTE_FILE_PATH = "src/main/resources/CommentStubDb.json";
 
     public CommentStubDb() {
-        loadComments();
+        loadJsonToArrayList();
     }
 
     @Override
@@ -25,40 +34,59 @@ public class CommentStubDb implements CommentDao {
 
     @Override
     public List<CommentDto> getCommentsByComic(long comicID) {
-        return null;
+        List<CommentDto> commentList = new ArrayList<>();
+        for(CommentDto c: commentsStubData){
+            if(c.getComicBookId() == comicID){
+                commentList.add(c);
+            }
+        }
+        return commentList;
     }
 
     @Override
     public void saveComment(CommentDto commentDTO) {
-
+        commentsStubData.add(commentDTO);
     }
 
     @Override
     public List<CommentDto> getCommentsByUser(long userID) {
-        return null;
+        List<CommentDto> commentList = new ArrayList<>();
+        for(CommentDto c: commentsStubData){
+            if(c.getUserId() == userID){
+                commentList.add(c);
+            }
+        }
+        return commentList;
     }
 
-    public void loadComments() {
 
-        CommentDto comment1 = new CommentDto(
-                "This comic was really good",
-                "Always_Scheming",
-                "Spider Man"
-        );
-        commentsStubData.add(comment1);
-
-        CommentDto comment2 = new CommentDto(
-                "This comic was really bad",
-                "Always_Throwing",
-                "Spider man"
-        );
-        commentsStubData.add(comment2);
-
-        CommentDto comment3 = new CommentDto(
-                "This comic changed my life",
-                "Phoenix",
-                "Arkham Asylum"
-        );
-        commentsStubData.add(comment3);
+    public void serializeUserStubDB() {
+        String json = new Gson().toJson(commentsStubData);
+        String file = "src/main/resources/UserStubDb.json";
+        try (PrintStream out = new PrintStream(new FileOutputStream(file))) {
+            out.print(json);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public List<CommentDto> deserializeUserStubDB(String filepath) {
+        Type listType = new TypeToken<ArrayList<CommentDto>>(){}.getType();
+        JsonReader reader = null;
+
+        try {
+            reader = new JsonReader(new FileReader(filepath));
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<CommentDto> dummyUsers = new Gson().fromJson(reader, listType);
+        return dummyUsers;
+    }
+
+    public void loadJsonToArrayList(){
+        commentsStubData = deserializeUserStubDB(ABSOLUTE_FILE_PATH);
+    }
+
 }
