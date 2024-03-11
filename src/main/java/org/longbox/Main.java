db509e90c81bdb4621fbc66d1d36976edb2f794d
ahan10 <ahanbhargava10@gmail.com>
2024-03-11 11:58:28 -0400

    [ADD] Added authentication frame call to Main.java

--- src/main/java/org/longbox/Main.java
+++ src/main/java/org/longbox/Main.java
@@ -1,16 +1,9 @@
 package org.longbox;
 
-import org.longbox.domainobjects.dto.ComicBookDto;
-import org.longbox.persistence.dao.ComicBookDaoImpl;
-import org.longbox.persistence.dao.ComicBookFinishedListDaoImpl;
-import org.longbox.persistence.dao.UserDaoImpl;
-import org.longbox.persistence.entity.ComicBook;
-import org.longbox.persistence.entity.User;
-import org.longbox.persistence.stubdatabase.ComicBookStubDb;
-
-import java.util.List;
+import org.longbox.presentation.authentication.AuthenticationFrame;
 
 public class Main {
     public static void main(String[] args) {
+        AuthenticationFrame authenticationFrame = new AuthenticationFrame();
     }
 }
