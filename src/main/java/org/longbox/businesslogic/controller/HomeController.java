34f16a33a2cd7b95efdf7c9c09959ad04168317f
ahan10 <ahanbhargava10@gmail.com>
2024-03-11 17:51:32 -0400

    [ADD] Added Controller class for Home Page

---
+++ src/main/java/org/longbox/businesslogic/controller/HomeController.java
@@ -0,0 +1,11 @@
+package org.longbox.businesslogic.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HomeController implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
