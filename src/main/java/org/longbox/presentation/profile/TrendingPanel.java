bbaf530e706427e9d71bae76300bdbac1c6f188f
SupaHacka02 <mherericgyulumyan@gmail.com>
2024-03-11 17:25:23 -0400

    [ADD] More detail to trending panel

--- src/main/java/org/longbox/presentation/profile/TrendingPanel.java
+++ src/main/java/org/longbox/presentation/profile/TrendingPanel.java
@@ -2,6 +2,11 @@ package org.longbox.presentation.profile;
 
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class TrendingPanel extends JPanel {
 
@@ -19,6 +24,23 @@ public class TrendingPanel extends JPanel {
 		
 		setBounds(10, 47, 1164, 803);
 		setLayout(new BorderLayout());
+		
+		JPanel panel = new JPanel();
+		add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 110, 1144, 683);
+		panel.add(scrollPane);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 92, 1144, 14);
+		panel.add(separator);
+		
+		JLabel trendingTitle = new JLabel("Trending");
+		trendingTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		trendingTitle.setFont(new Font("Calibri", Font.PLAIN, 30));
+		trendingTitle.setBounds(396, 11, 372, 43);
+		panel.add(trendingTitle);
 	}
-
 }
