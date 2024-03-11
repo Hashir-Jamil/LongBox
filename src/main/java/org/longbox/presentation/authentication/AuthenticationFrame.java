adfe810920d00f2894e18e95c0e19f59aee9d826
ahan10 <ahanbhargava10@gmail.com>
2024-03-11 17:54:26 -0400

    [REFACTOR] Removed unused import statements

--- src/main/java/org/longbox/presentation/authentication/AuthenticationFrame.java
+++ src/main/java/org/longbox/presentation/authentication/AuthenticationFrame.java
@@ -1,24 +1,14 @@
 package org.longbox.presentation.authentication;
 
 import java.awt.CardLayout;
-import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.longbox.businesslogic.UserSession;
-import org.longbox.businesslogic.exception.EmailDoesNotExistException;
-import org.longbox.businesslogic.exception.UserNameDoesNotExistException;
-import org.longbox.businesslogic.exception.UsernameOrEmailExistsException;
-import org.longbox.domainobjects.dto.UserDto;
-import org.longbox.persistence.dao.UserDaoImpl;
-import org.longbox.persistence.entity.User;
-import org.longbox.presentation.profile.HomeFrame;
+
 @Getter
 @Setter
 public class AuthenticationFrame extends JFrame {
@@ -45,9 +35,6 @@ public class AuthenticationFrame extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public AuthenticationFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 800, 550);
@@ -69,8 +56,6 @@ public class AuthenticationFrame extends JFrame {
         setVisible(true);
 
 	}
-
-
 
 	public void showLogin(){
 		cardLayout.show(cardPanel, "login");
