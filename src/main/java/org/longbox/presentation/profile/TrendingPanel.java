623d384e451101826471bb1c63220baa7721fce1
SupaHacka02 <mherericgyulumyan@gmail.com>
2024-03-11 22:49:45 -0400

    [ADD] Trending panel now has national favorites
    (currently filled with all time favorites, needs to work on national
    favorites functionality)

--- src/main/java/org/longbox/presentation/profile/TrendingPanel.java
+++ src/main/java/org/longbox/presentation/profile/TrendingPanel.java
@@ -13,11 +13,14 @@ import org.longbox.persistence.dao.ComicBookDaoImpl;
 import org.longbox.presentation.tablemodels.TrendingAllTimeTableModel;
 
 import javax.swing.JTable;
+import java.awt.Component;
+import javax.swing.ScrollPaneConstants;
 
 public class TrendingPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private JTable comicTable;
+	private JTable allTimeFavoritesTable;
+	private JTable nationalFavoritesTable;
 
 	/**
 	 * Create the panel.
@@ -41,6 +44,20 @@ public class TrendingPanel extends JPanel {
 		trendingTitle.setFont(new Font("Calibri", Font.PLAIN, 30));
 		trendingTitle.setBounds(396, 11, 372, 43);
 		panel.add(trendingTitle);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 92, 1144, 14);
+		panel.add(separator);
+		
+		JLabel allTimeFavoritesLabel = new JLabel("All Time Favorites:");
+		allTimeFavoritesLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		allTimeFavoritesLabel.setBounds(10, 106, 117, 22);
+		panel.add(allTimeFavoritesLabel);
+		
+		JLabel nationalFavorites = new JLabel("National Favorites: ");
+		nationalFavorites.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		nationalFavorites.setBounds(10, 450, 117, 22);
+		panel.add(nationalFavorites);
 			
 		ComicBookDaoImpl comicBookDaoImpl = new ComicBookDaoImpl();
 		TrendingAllTimeTableModel comicBookTableModel = new TrendingAllTimeTableModel(comicBookDaoImpl.getAllComicBooks());
@@ -49,17 +66,21 @@ public class TrendingPanel extends JPanel {
 			System.out.println(c + " "+ c.getFavoritesCount());
 		}
 		
-		comicTable = new JTable(comicBookTableModel);
-		comicTable.setBounds(0, 0, 1, 1);
-		panel.add(comicTable);
+		allTimeFavoritesTable = new JTable(comicBookTableModel);
+		allTimeFavoritesTable.setBounds(0, 0, 1, 1);
+		panel.add(allTimeFavoritesTable);
 		
-		JScrollPane scrollPane = new JScrollPane(comicTable);
-		scrollPane.setBounds(10, 110, 1144, 683);
-		panel.add(scrollPane);
+		nationalFavoritesTable = new JTable(comicBookTableModel);
+		nationalFavoritesTable.setBounds(0, 0, 1, 1);
+		panel.add(nationalFavoritesTable);
 		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(10, 92, 1144, 14);
-		panel.add(separator);
+		JScrollPane allTimeFavoritesScrollPane = new JScrollPane(allTimeFavoritesTable);
+		allTimeFavoritesScrollPane.setBounds(10, 139, 1144, 300);
+		panel.add(allTimeFavoritesScrollPane);
+		
+		JScrollPane nationalFavoritesScrollPane = new JScrollPane(nationalFavoritesTable);
+		nationalFavoritesScrollPane.setBounds(10, 483, 1144, 300);
+		panel.add(nationalFavoritesScrollPane);
 		
 		
 	}
