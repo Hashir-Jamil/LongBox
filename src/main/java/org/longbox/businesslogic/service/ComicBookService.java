56cd3dcfd216b6f37a7d3aae0ef485acf569dcb6
Hashir Jamil <hashir.jamil17@gmail.com>
2024-03-12 15:44:44 -0400

    [ADD] service layer classes for comic books and comments

--- src/main/java/org/longbox/businesslogic/service/ComicBookService.java
+++ src/main/java/org/longbox/businesslogic/service/ComicBookService.java
@@ -1,7 +1,36 @@
 package org.longbox.businesslogic.service;
 
+import org.longbox.domainobjects.dto.ComicBookDto;
+import org.longbox.persistence.dao.ComicBookDao;
+import org.longbox.persistence.entity.ComicBook;
+
+import java.util.List;
+
 public class ComicBookService {
 
+    ComicBookDao comicBookDao;
 
+    public ComicBookService(ComicBookDao comicBookDao) {
+        this.comicBookDao = comicBookDao;
+    }
 
+    public ComicBookDto getComicBookById(Long comicId) {
+        return new ComicBookDto(comicBookDao.getComicBookById(comicId));
+    }
+
+    public ComicBookDto getComicBookBySeriesName(String seriesTitle) {
+        return new ComicBookDto(comicBookDao.getComicBookBySeriesTitle(seriesTitle));
+    }
+
+    public Long saveComicBook(ComicBookDto comicBookDto) {
+        return comicBookDao.saveComicBook(new ComicBook(comicBookDto));
+    }
+
+    public boolean deleteComicBook(ComicBookDto comicBookDto) {
+        return false;
+    }
+
+    public List<ComicBookDto> getAllComicBook() {
+        return comicBookDao.getAllComicBooks();
+    }
 }
--- src/main/java/org/longbox/businesslogic/service/CommentService.java
+++ src/main/java/org/longbox/businesslogic/service/CommentService.java
@@ -1,4 +1,28 @@
 package org.longbox.businesslogic.service;
 
+import org.longbox.domainobjects.dto.CommentDto;
+import org.longbox.persistence.dao.CommentDao;
+
+import java.util.List;
+
 public class CommentService {
+
+    CommentDao commentDao;
+
+    public CommentService(CommentDao commentDao) {
+        this.commentDao = commentDao;
+    }
+
+    public List<CommentDto> getCommentsByComic(Long comicId) {
+        return commentDao.getCommentsByComic(comicId);
+    }
+
+    public void saveComment(CommentDto commentDto) {
+        commentDao.saveComment(commentDto);
+    }
+
+    public List<CommentDto> getCommentsByUser(Long userId) {
+        return commentDao.getCommentsByUser(userId);
+    }
+
 }
