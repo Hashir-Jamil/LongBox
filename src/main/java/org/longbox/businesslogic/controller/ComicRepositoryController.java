8512d9b51147b115ada9254b3475b2fed7b9dfc5
SupaHacka02 <mherericgyulumyan@gmail.com>
2024-03-14 19:57:46 -0400

    [ADD] Basic functionality for comic repo controller

--- src/main/java/org/longbox/businesslogic/controller/ComicRepositoryController.java
+++ src/main/java/org/longbox/businesslogic/controller/ComicRepositoryController.java
@@ -1,4 +1,30 @@
 package org.longbox.businesslogic.controller;
 
-public class ComicRepositoryController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import org.longbox.businesslogic.UserSession;
+import org.longbox.businesslogic.service.UserService;
+import org.longbox.presentation.profile.ComicRepositoryPanel;
+
+public class ComicRepositoryController implements ActionListener {
+	
+	private ComicRepositoryPanel comicRepositoryPanel;
+	private UserSession userSession;
+    private UserService userService;
+	
+	public ComicRepositoryController(ComicRepositoryPanel comicRepositoryPanel) {
+		this.comicRepositoryPanel = comicRepositoryPanel;
+		this.userSession = comicRepositoryPanel.getUserSession();
+	}
+	
+	private void addListeners() {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
