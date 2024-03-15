aa7457d3035a1b0dc5215ba07228f1ca8d89541e
SupaHacka02 <mherericgyulumyan@gmail.com>
2024-03-14 22:10:27 -0400

    [REFACTOR] removed unused code

--- src/main/java/org/longbox/presentation/profile/ComicRepositoryPanel.java
+++ src/main/java/org/longbox/presentation/profile/ComicRepositoryPanel.java
@@ -78,28 +78,11 @@ public class ComicRepositoryPanel extends JPanel {
 
 		add(panel, BorderLayout.CENTER);
 		
-//		this.reloadTable();
-		
 		comicBookDaoImpl = new ComicBookDaoImpl(HibernateUtils.getSessionFactory());
 
 		comicBookTableModel = new ComicBookTableModel(comicBookDaoImpl.getAllComicBooks());
 
 		comicBookTable = new JTable(comicBookTableModel);
-//		comicBookTable.addMouseListener(new MouseAdapter() {
-//			@Override
-//			public void mouseClicked(MouseEvent e) {
-//				int selectedRow = comicBookTable.getSelectedRow();
-//				if (selectedRow != -1) {
-//					addToFavoritesButton.setEnabled(true);
-//				}
-//				int row = comicBookTable.rowAtPoint(e.getPoint());
-//				int col = comicBookTable.columnAtPoint(e.getPoint());
-//				if (col == 0 && e.getClickCount() == 2) {
-//					ComicBookDto comicBook = org.longbox.businesslogic.utils.ComicBookSearchUtils.searchComicBook(comicBookDaoImpl.getAllComicBooks(), comicBookTable.getValueAt(row, col).toString());
-//					ComicBookSearchUtils.loadComicBookPage(comicBook, userSession);
-//				}
-//			}
-//		});
 		sorter = new TableRowSorter<TableModel>(comicBookTable.getModel());
 		comicBookTable.setRowSorter(sorter);
 
@@ -127,8 +110,6 @@ public class ComicRepositoryPanel extends JPanel {
 		panel.add(textField);
 		textField.setColumns(10);
 		
-		//textField.addActionListener(this);
-		
 		JLabel lblNewLabel = new JLabel("Search Repo:");
 		lblNewLabel.setBounds(10, 66, 120, 13);
 		panel.add(lblNewLabel);
@@ -136,90 +117,11 @@ public class ComicRepositoryPanel extends JPanel {
 		refreshButton = new JButton("Refresh");
 		refreshButton.setForeground(Color.BLACK);
 		refreshButton.setBounds(1065, 62, 89, 23);
-//		refreshButton.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				panel.remove(scrollPane);
-//				//reloadTable();
-//			}
-//		});
 		panel.add(refreshButton);
 
 		addToFavoritesButton = new JButton("Add to Favorites");
 		addToFavoritesButton.setBounds(930, 62, 129, 23);
 		addToFavoritesButton.setEnabled(false); // Initially inactive
-		//addToFavoritesButton.addActionListener(this);
 		panel.add(addToFavoritesButton);
 	}
-
-//	@Override
-//    public void actionPerformed(ActionEvent e) {
-//		if (e.getSource() == textField && !textField.getText().isEmpty()) {
-//			String searchBy = typeSelection.getSelectedItem().toString();
-//			String target = textField.getText();
-//			List<ComicBookDto> searchResults = null;
-//			searchResults = org.longbox.businesslogic.utils.ComicBookSearchUtils.comicAdvancedSearch(searchBy, target, searchResults, comicBookDaoImpl.getAllComicBooks(), this.userSession);
-//		} else if (e.getSource() == addToFavoritesButton) {
-//			int selectedRow = comicBookTable.getSelectedRow();
-//			if (selectedRow != -1) {
-//				long comicId = comicBookTableModel.getComicIdAtRow(selectedRow);
-//				// Check if the comic book is already in favorites list
-//				if (!isComicInFavorites(comicId)) {
-//                    try {
-//                        addComicToFavorites(comicId);
-//                    } catch (UserIDDoesNotExistException ex) {
-//                        throw new RuntimeException(ex);
-//                    }
-//                    // Disable the button after adding to favorites
-//					addToFavoritesButton.setEnabled(false);
-//				}
-//			}
-//		}
-//    }
-
-//	private boolean isComicInFavorites(long comicId) {
-//		ComicBook comicBook = comicBookDaoImpl.getComicBookById(comicId);
-//		ComicBookDto comicBookDTO = new ComicBookDto(comicBook);
-//		System.out.println("the comic in favorites is " + comicBookFavouritesListDaoImpl.getAllFavoritesComicBooks().contains(comicBookDTO));
-//		return comicBookFavouritesListDaoImpl.getAllFavoritesComicBooks().contains(comicBookDTO);
-//	}
-//
-//	private void addComicToFavorites(long comicId) throws UserIDDoesNotExistException {
-//		FavoritesPanel favoritesPanel = new FavoritesPanel();
-//		comicBookDaoImpl.favoriteComicBook(comicId);
-//		favoritesPanel.update(this.userSession.getUser().getId(), comicId);
-//		favoritesPanel.reloadData();
-//	}
-
-//	public void reloadTable() {
-//		comicBookDaoImpl = new ComicBookDaoImpl(HibernateUtils.getSessionFactory());
-//
-//		comicBookTableModel = new ComicBookTableModel(comicBookDaoImpl.getAllComicBooks());
-//
-//		comicBookTable = new JTable(comicBookTableModel);
-//		comicBookTable.addMouseListener(new MouseAdapter() {
-//			@Override
-//			public void mouseClicked(MouseEvent e) {
-//				int selectedRow = comicBookTable.getSelectedRow();
-//				if (selectedRow != -1) {
-//					addToFavoritesButton.setEnabled(true);
-//				}
-//				int row = comicBookTable.rowAtPoint(e.getPoint());
-//				int col = comicBookTable.columnAtPoint(e.getPoint());
-//				if (col == 0 && e.getClickCount() == 2) {
-//					ComicBookDto comicBook = org.longbox.businesslogic.utils.ComicBookSearchUtils.searchComicBook(comicBookDaoImpl.getAllComicBooks(), comicBookTable.getValueAt(row, col).toString());
-//					ComicBookSearchUtils.loadComicBookPage(comicBook, userSession);
-//				}
-//			}
-//		});
-//		sorter = new TableRowSorter<TableModel>(comicBookTable.getModel());
-//		comicBookTable.setRowSorter(sorter);
-//
-//		scrollPane = new JScrollPane(comicBookTable);
-//		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-//		scrollPane.setViewportBorder(null);
-//		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-//		scrollPane.setBounds(10, 110, 1144, 683);
-//		panel.add(scrollPane);
-//	}
 }
