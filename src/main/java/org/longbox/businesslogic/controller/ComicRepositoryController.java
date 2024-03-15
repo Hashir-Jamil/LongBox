035f1efc52b2072452fd4e30273c96558cd4a685
SupaHacka02 <mherericgyulumyan@gmail.com>
2024-03-14 22:07:50 -0400

    [HOTFIX] Fixed improper comic favorited from comic repo panel

--- src/main/java/org/longbox/businesslogic/controller/ComicRepositoryController.java
+++ src/main/java/org/longbox/businesslogic/controller/ComicRepositoryController.java
@@ -28,6 +28,7 @@ import org.longbox.presentation.profile.FavoritesPanel;
 public class ComicRepositoryController implements ActionListener, MouseListener {
 	
 	private ComicRepositoryPanel comicRepositoryPanel;
+	private ComicBookDaoImpl comicBookDaoImpl = new ComicBookDaoImpl(HibernateUtils.getSessionFactory());;
 	private UserSession userSession;
     private UserService userService;
 	
@@ -62,7 +63,14 @@ public class ComicRepositoryController implements ActionListener, MouseListener 
 		if(e.getSource() == this.comicRepositoryPanel.getAddToFavoritesButton()) {
 			int selectedRow = this.comicRepositoryPanel.getComicBookTable().getSelectedRow();
 			if (selectedRow != -1) {
-				long comicId = this.comicRepositoryPanel.getComicBookTableModel().getComicIdAtRow(selectedRow);
+//				long comicId = this.comicRepositoryPanel.getComicBookTableModel().getComicIdAtRow(selectedRow);
+				
+				int row = this.comicRepositoryPanel.getComicBookTable().getSelectedRow();
+				int col = this.comicRepositoryPanel.getComicBookTable().getSelectedColumn();
+				String name = this.comicRepositoryPanel.getComicBookTable().getValueAt(row, col).toString();
+				long comicId = comicBookDaoImpl.getComicBookBySeriesTitle(name).getId();
+				
+				System.out.println("HERE WE HAVE THE NAME: " + name);
 				// Check if the comic book is already in favorites list
 				if (!isComicInFavorites(comicId)) {
                     try {
