97c2d04dede9ccdb410508fa363bbe7f0cbe1a03
SupaHacka02 <mherericgyulumyan@gmail.com>
2024-03-16 19:16:45 -0400

    [REFACTOR] Changed variables of trending panel and added trending controller

---
+++ src/main/java/org/longbox/businesslogic/controller/TrendingController.java
@@ -0,0 +1,13 @@
+package org.longbox.businesslogic.controller;
+
+import org.longbox.presentation.profile.TrendingPanel;
+
+public class TrendingController {
+
+	private TrendingPanel trendingPanel;
+	
+	public TrendingController(TrendingPanel trendingPanel) {
+		this.trendingPanel = trendingPanel;
+	}
+
+}
--- src/main/java/org/longbox/presentation/profile/TrendingPanel.java
+++ src/main/java/org/longbox/presentation/profile/TrendingPanel.java
@@ -5,6 +5,8 @@ import org.longbox.domainobjects.dto.ComicBookDto;
 import org.longbox.persistence.dao.ComicBookDaoImpl;
 import org.longbox.presentation.tablemodels.TrendingAllTimeTableModel;
 
+import lombok.*;
+
 import java.awt.BorderLayout;
 import java.awt.Font;
 import javax.swing.JScrollPane;
@@ -14,9 +16,21 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.JTable;
 
+@Setter
+@Getter
 public class TrendingPanel extends JPanel {
+	
 	private JTable allTimeFavoritesTable;
 	private JTable nationalFavoritesTable;
+	private JPanel panel;
+	private JLabel trendingTitle;
+	private JSeparator separator;
+	private JLabel allTimeFavoritesLabel;
+	private JLabel nationalFavorites;
+	private ComicBookDaoImpl comicBookDaoImpl;
+	private TrendingAllTimeTableModel comicBookTableModel;
+	private JScrollPane allTimeFavoritesScrollPane;
+	private JScrollPane nationalFavoritesScrollPane;
 
 	public TrendingPanel() {
 		initTrendingPanel();
@@ -27,32 +41,32 @@ public class TrendingPanel extends JPanel {
 		setBounds(10, 47, 1164, 803);
 		setLayout(new BorderLayout());
 		
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
-		JLabel trendingTitle = new JLabel("Trending");
+		trendingTitle = new JLabel("Trending");
 		trendingTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		trendingTitle.setFont(new Font("Calibri", Font.PLAIN, 30));
 		trendingTitle.setBounds(396, 11, 372, 43);
 		panel.add(trendingTitle);
 		
-		JSeparator separator = new JSeparator();
+		separator = new JSeparator();
 		separator.setBounds(10, 92, 1144, 14);
 		panel.add(separator);
 		
-		JLabel allTimeFavoritesLabel = new JLabel("All Time Favorites:");
+		allTimeFavoritesLabel = new JLabel("All Time Favorites:");
 		allTimeFavoritesLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		allTimeFavoritesLabel.setBounds(10, 106, 117, 22);
 		panel.add(allTimeFavoritesLabel);
 		
-		JLabel nationalFavorites = new JLabel("National Favorites: ");
+		nationalFavorites = new JLabel("National Favorites: ");
 		nationalFavorites.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		nationalFavorites.setBounds(10, 450, 117, 22);
 		panel.add(nationalFavorites);
 			
-		ComicBookDaoImpl comicBookDaoImpl = new ComicBookDaoImpl(HibernateUtils.getSessionFactory());
-		TrendingAllTimeTableModel comicBookTableModel = new TrendingAllTimeTableModel(comicBookDaoImpl.getAllComicBooks());
+		comicBookDaoImpl = new ComicBookDaoImpl(HibernateUtils.getSessionFactory());
+		comicBookTableModel = new TrendingAllTimeTableModel(comicBookDaoImpl.getAllComicBooks());
 		
 		for(ComicBookDto c : comicBookDaoImpl.getAllComicBooks()) {
 			System.out.println(c + " "+ c.getFavoritesCount());
@@ -66,11 +80,11 @@ public class TrendingPanel extends JPanel {
 		nationalFavoritesTable.setBounds(0, 0, 1, 1);
 		panel.add(nationalFavoritesTable);
 		
-		JScrollPane allTimeFavoritesScrollPane = new JScrollPane(allTimeFavoritesTable);
+		allTimeFavoritesScrollPane = new JScrollPane(allTimeFavoritesTable);
 		allTimeFavoritesScrollPane.setBounds(10, 139, 1144, 300);
 		panel.add(allTimeFavoritesScrollPane);
 		
-		JScrollPane nationalFavoritesScrollPane = new JScrollPane(nationalFavoritesTable);
+		nationalFavoritesScrollPane = new JScrollPane(nationalFavoritesTable);
 		nationalFavoritesScrollPane.setBounds(10, 483, 1144, 300);
 		panel.add(nationalFavoritesScrollPane);
 	}
