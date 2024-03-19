c938d5b1472f3ecfc9584354d8f613d572110f42
ahan10 <ahanbhargava10@gmail.com>
2024-03-18 23:47:12 -0400

    [REFACTOR] Moved conversion of String to Array and vice versa from ComicBookDto to Utils

--- src/main/java/org/longbox/domainobjects/dto/ComicBookDto.java
+++ src/main/java/org/longbox/domainobjects/dto/ComicBookDto.java
@@ -240,25 +240,4 @@ public class ComicBookDto {
                 ", favoritesCount=" + favoritesCount +
                 '}';
     }
-
-    @Deprecated
-    public static String[] genreStringToList(String genres) {
-        return genres.split(",\\s*");
-    }
-
-    @Deprecated
-    public static String genreListToString(String[] genresList) {
-        String genres = "";
-        if (genresList.length == 0) {
-            return genres;
-        }
-        if (genresList.length == 1) {
-            return genresList[0];
-        }
-        for (int i = 0; i < genresList.length - 1; i++) {
-            genres = genres + genresList[i] + ", ";
-        }
-        genres = genres + genresList[genresList.length - 1];
-        return genres;
-    }
 }
