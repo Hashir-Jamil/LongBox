0ae1b2d07f93ad7db13b87aba79229508c945a2e
ahan10 <ahanbhargava10@gmail.com>
2024-03-21 17:42:16 -0400

    [HOTFIX] Tables no longer editable

--- src/main/java/org/longbox/presentation/tablemodels/UsersTableModel.java
+++ src/main/java/org/longbox/presentation/tablemodels/UsersTableModel.java
@@ -1,11 +1,13 @@
 package org.longbox.presentation.tablemodels;
 
 import java.util.List;
+import lombok.*;
 
 import javax.swing.table.DefaultTableModel;
 
 import org.longbox.domainobjects.dto.UserDto;
 
+@NoArgsConstructor
 public class UsersTableModel extends DefaultTableModel {
 	private static final long serialVersionUID = 1L;
 	List<UserDto> userList;
@@ -32,5 +34,13 @@ public class UsersTableModel extends DefaultTableModel {
 				u.getCountry()
 			});
 		}
+	}
+	@Override
+	public boolean isCellEditable(int row, int column) {
+		return false;
+	}
+
+	public List<UserDto> getDisplayList() {
+		return this.userList;
 	}
 }
