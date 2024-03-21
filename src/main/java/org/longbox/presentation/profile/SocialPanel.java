674d4c2d7b6cb674132e6d412ce54156bc307a62
ahan10 <ahanbhargava10@gmail.com>
2024-03-21 17:43:21 -0400

    [ADD] Add mouse adapter

--- src/main/java/org/longbox/presentation/profile/SocialPanel.java
+++ src/main/java/org/longbox/presentation/profile/SocialPanel.java
@@ -2,24 +2,37 @@ package org.longbox.presentation.profile;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.*;
+import lombok.*;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import org.longbox.businesslogic.service.UserService;
+import org.longbox.businesslogic.utils.UserSearchUtils;
 import org.longbox.config.HibernateUtils;
 import org.longbox.domainobjects.dto.UserDto;
 import org.longbox.persistence.dao.UserDaoImpl;
+import org.longbox.presentation.otheruser.OtherUserProfileFrame;
 import org.longbox.presentation.tablemodels.UsersTableModel;
 
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JTable;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
+@Getter
+@Setter
 public class SocialPanel extends JPanel {
 
+	private static final String RESET_TEXT = "Reset";
+	private static final String TITLE_TEXT = "Nerd Hall of Fame";
+	private static final String FILTER = "Filter";
 	private static final long serialVersionUID = 1L;
 	
 	private List<UserDto> allUsersList;
@@ -31,12 +44,13 @@ public class SocialPanel extends JPanel {
 	private UsersTableModel usersTableModel;
 	private JScrollPane scrollPane;
 	private JTable allUsersTable;
+	private JButton filterButton;
+	private JComboBox numberComboBox, more_less_thanComboBox, choiceComboBox;
+	private JButton resetButton;
 	
 	public SocialPanel() {
 		userService = new UserService(new UserDaoImpl(HibernateUtils.getSessionFactory()));
-		
-		this.allUsersList = userService.getAllUsers();
-		
+
 		initComponents();
 	}
 	
@@ -52,24 +66,87 @@ public class SocialPanel extends JPanel {
 	}
 	
 	private void addLabels() {
-		titleLabel = new JLabel("Nerd Hall of Fame");
+		titleLabel = new JLabel(TITLE_TEXT);
 		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		titleLabel.setFont(new Font("Calibri", Font.PLAIN, 30));
 		titleLabel.setBounds(396, 11, 372, 43);
 		panel.add(titleLabel);
 		
 		separator = new JSeparator();
-		separator.setBounds(10, 92, 1144, 14);
+		separator.setBounds(0, 66, 1144, 14);
 		panel.add(separator);
 		
-		usersTableModel = new UsersTableModel(allUsersList);
+		filterButton = new JButton(FILTER);
+		filterButton.setBounds(1048, 90, 85, 29);
+		panel.add(filterButton);
 		
-		allUsersTable = new JTable(usersTableModel);
-		allUsersTable.setBounds(0, 0, 1, 1);
-		panel.add(allUsersTable);
+		resetButton = new JButton(RESET_TEXT);
+		resetButton.setBounds(1016, 749, 117, 29);
+		panel.add(resetButton);
 		
-		scrollPane = new JScrollPane(allUsersTable);
-		scrollPane.setBounds(29, 120, 1104, 653);
-		panel.add(scrollPane);
+		numberComboBox = new JComboBox();
+		numberComboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "15", "20", "25"}));
+		numberComboBox.setBounds(974, 91, 71, 27);
+		panel.add(numberComboBox);
+		
+		more_less_thanComboBox = new JComboBox();
+		more_less_thanComboBox.setModel(new DefaultComboBoxModel(new String[] {"more than", "less than"}));
+		more_less_thanComboBox.setBounds(829, 91, 133, 27);
+		panel.add(more_less_thanComboBox);
+		
+		choiceComboBox = new JComboBox();
+		choiceComboBox.setModel(new DefaultComboBoxModel(new String[] {"Reading", "Finished"}));
+		choiceComboBox.setBounds(705, 91, 112, 27);
+		panel.add(choiceComboBox);
+
+		reset();
 	}
+	
+	public void addElementsToTable(List<UserDto> displayList) {
+
+		usersTableModel = new UsersTableModel(displayList);
+		allUsersTable = new JTable(usersTableModel);
+
+		allUsersTable.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int row = allUsersTable.rowAtPoint(e.getPoint());
+				int col = allUsersTable.columnAtPoint(e.getPoint());
+				if (col == 0 && e.getClickCount() == 2) {
+					System.out.println("\n mouse clicked \n");
+					UserDto userDto = UserSearchUtils.getSearchedUser(displayList, allUsersTable.getValueAt(row, col).toString());
+					new OtherUserProfileFrame(userDto);
+				}
+			}
+		});
+
+		scrollPane = new JScrollPane(allUsersTable);
+		scrollPane.setBounds(29, 120, 1104, 624);
+		panel.add(scrollPane);
+
+
+	}
+	
+	public void reset() {
+		this.allUsersList = userService.getAllUsers();
+		addElementsToTable(allUsersList);
+	}
+	
+	public int getNumberSelected() {
+		return Integer.parseInt(numberComboBox.getSelectedItem().toString());
+	}
+	
+	public String get_more_less_thanSelected() {
+		return more_less_thanComboBox.getSelectedItem().toString();
+	}
+	
+	public String getChoiceSelected() {
+		if(choiceComboBox.getSelectedItem().toString().equals("Reading")){
+			return "comicsReading";
+		} else if(choiceComboBox.getSelectedItem().toString().equals("Finished")){
+			return "comicsFinished";
+		}
+		return null;
+	}
+
 }
