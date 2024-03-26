ef992b6d08a1abb315d06f82d3c9e87723e456b2
SupaHacka02 <mherericgyulumyan@gmail.com>
2024-03-26 17:17:42 -0400

    [ADD] RecommendationsController

---
+++ src/main/java/org/longbox/businesslogic/controller/RecommendationsController.java
@@ -0,0 +1,47 @@
+package org.longbox.businesslogic.controller;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class RecommendationsController implements MouseListener {
+
+	public RecommendationsController() {
+		// TODO Auto-generated constructor stub
+		loadListeners();
+	}
+	
+	private void loadListeners() {
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
