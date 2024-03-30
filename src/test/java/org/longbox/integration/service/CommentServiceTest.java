ea873efee7216add525afc2a3d6fdd3caa96d076
ahan10 <ahanbhargava10@gmail.com>
2024-03-30 00:50:49 -0400

    [REFACTOR] Moved service tests from unit to integration.

--- src/test/java/org/longbox/integration/service/CommentServiceTest.java
+++ src/test/java/org/longbox/integration/service/CommentServiceTest.java
@@ -1,4 +1,73 @@
 package org.longbox.integration.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.longbox.businesslogic.service.CommentService;
+import org.longbox.config.HibernateUtils;
+import org.longbox.domainobjects.dto.CommentDto;
+import org.longbox.persistence.dao.CommentDaoImpl;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CommentServiceTest {
+    CommentService commentService;
+
+    @BeforeEach
+    void init() {
+        commentService = new CommentService(new CommentDaoImpl(HibernateUtils.getSessionFactory()));
+    }
+
+    @Test
+    void test_getCommentsByComic_1() {
+        List<CommentDto> actualList = commentService.getCommentsByComic(1L);
+        int actual = actualList.size();
+        assertEquals(2, actual);
+    }
+
+    @Test
+    void test_getCommentsByComic_2() {
+        List<CommentDto> actualList = commentService.getCommentsByComic(14L);
+        int actual = actualList.size();
+        assertEquals(3, actual);
+    }
+
+    @Test
+    void test_getCommentsByComic_3() {
+        List<CommentDto> actualList = commentService.getCommentsByComic(23L);
+        int actual = actualList.size();
+        assertEquals(0, actual);
+    }
+
+    @Test
+    void test_saveComment(){
+        CommentDto addComment = new CommentDto();
+        addComment.setUserId(3);
+        addComment.setComicBookId(13);
+        addComment.setMessage("This is a nice comic");
+        addComment.setUserName("Phoenix");
+        commentService.saveComment(addComment);
+    }
+
+    @Test
+    void test_getCommentByUser_1(){
+        List<CommentDto> actualList = commentService.getCommentsByUser(1L);
+        int actual = actualList.size();
+        assertEquals(10, actual);
+    }
+
+    @Test
+    void test_getCommentByUser_2(){
+        List<CommentDto> actualList = commentService.getCommentsByUser(2L);
+        int actual = actualList.size();
+        assertEquals(9, actual);
+    }
+
+    @Test
+    void test_getCommentByUser_3(){
+        List<CommentDto> actualList = commentService.getCommentsByUser(5L);
+        int actual = actualList.size();
+        assertEquals(4, actual);
+    }
 }
--- src/test/java/org/longbox/integration/service/StarRatingServiceTest.java
+++ src/test/java/org/longbox/integration/service/StarRatingServiceTest.java
@@ -1,4 +1,71 @@
 package org.longbox.integration.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.longbox.businesslogic.service.StarRatingService;
+import org.longbox.config.HibernateUtils;
+import org.longbox.domainobjects.dto.StarRatingDto;
+import org.longbox.persistence.dao.StarRatingDaoImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StarRatingServiceTest {
+    StarRatingService starRatingService;
+
+    @BeforeEach
+    void setup() {
+        starRatingService = new StarRatingService(new StarRatingDaoImpl(HibernateUtils.getSessionFactory()));
+    }
+
+    @Test
+    void test_getStarRatingsByComic_1() {
+        String actualAvgStarRating = starRatingService.getAvgStarRatingsByComic(1L);
+        assertEquals("3.7", actualAvgStarRating);
+    }
+
+    @Test
+    void test_getStarRatingsByComic_2() {
+        String actualAvgStarRating = starRatingService.getAvgStarRatingsByComic(11L);
+        assertEquals("3.5", actualAvgStarRating);
+    }
+
+    @Test
+    void test_getStarRatingsByComic_3() {
+        String actualAvgStarRating = starRatingService.getAvgStarRatingsByComic(6L);
+        assertEquals("3", actualAvgStarRating);
+    }
+
+    @Test
+    void test_ratingNotFound() {
+        String starRating = starRatingService.getStarRatingByID(101L,100L);
+        assertEquals("Not rated",starRating);
+    }
+
+    @Test
+    void test_saveStarRating() {
+        StarRatingDto addStarRating = new StarRatingDto();
+        addStarRating.setUserId(1);
+        addStarRating.setComicBookId(1);
+        addStarRating.setRating(5);
+        starRatingService.saveStarRating(addStarRating);
+        assertEquals(starRatingService.getStarRatingByID(1L, 1L), "5");
+    }
+
+    @Test
+    void test_getStarRatingbyId_1() {
+        String actualStarRating = starRatingService.getStarRatingByID(1L, 1L);
+        assertEquals(actualStarRating, "5");
+    }
+
+    @Test
+    void test_getStarRatingbyId_2() {
+        String actualStarRating = starRatingService.getStarRatingByID(3L, 1L);
+        assertEquals(actualStarRating, "3");
+    }
+
+    @Test
+    void test_getStarRatingbyId_3() {
+        String actualStarRating = starRatingService.getStarRatingByID(25L, 2L);
+        assertEquals(actualStarRating, "5");
+    }
 }
